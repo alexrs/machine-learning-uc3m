8d899d4300db19159c63a47c0a2ca27b655e417b
Alejandro Rodriguez <alexrs95@gmail.com>
2016-03-07 17:54:07 +0100

    Update framerate in script

--- P1/run.sh
+++ P1/run.sh
@@ -2,6 +2,6 @@
 for fname in layouts/bigHunt.lay layouts/20Hunt.lay layouts/openClassic.lay layouts/openHunt.lay
 do
 	for i in 1 2 3 4 5; do
-		python busters.py -p GreedyBustersAgent -l -s $fname
+		python busters.py -p GreedyBustersAgent -l -t 0.005 $fname
 	done
 done
