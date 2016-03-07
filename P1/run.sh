6ee45bab375220a0d2453a60bcf53c7cafceec0e
dani <danielmedinagarcia@protonmail.com>
2016-03-07 21:49:00 +0100

    Automagic agent data added

--- P1/run.sh
+++ P1/run.sh
@@ -2,6 +2,23 @@
 for fname in layouts/bigHunt.lay layouts/20Hunt.lay layouts/openClassic.lay layouts/openHunt.lay
 do
 	for i in 1 2 3 4 5; do
-		python busters.py -p GreedyBustersAgent -l -t 0.005 $fname
+		python busters.py -p GreedyBustersAgent -l $fname -t 0.005 -q
 	done
 done
+mv data/game.arff data/training_tutorial1.arff
+
+for fname in layouts/bigHunt.lay layouts/20Hunt.lay layouts/openClassic.lay layouts/openHunt.lay
+do
+	for i in 1 2; do
+		python busters.py -p GreedyBustersAgent -l $fname -t 0.005 -q
+	done
+done
+mv data/game.arff data/test_samemaps_tutorial1.arff
+
+for fname in layouts/trappedClassic.lay layouts/sixHunt.lay
+do
+	for i in 1 2 3; do
+		python busters.py -p GreedyBustersAgent -l $fname -t 0.005 -q
+	done
+done
+mv data/game.arff data/test_othermaps_tutorial1.arff
