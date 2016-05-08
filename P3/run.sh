752af4e00dc94fd7abbeb27af547ab028b387e0a
dso <alexrs95@gmail.com>
2016-05-08 23:12:00 +0200

    qtable old agent

--- P3/run.sh
+++ P3/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 COUNTER=0
-while [ $COUNTER -lt 20 ]; do
+while [ $COUNTER -lt 30 ]; do
 	python busters.py -p P3QLearning -s
 	COUNTER=$((COUNTER+1))
 done
 COUNTER=0
-while [ $COUNTER -lt 20 ]; do
+while [ $COUNTER -lt 30 ]; do
 	python busters.py -p P3QLearning -l layouts/finalMap.lay -s
 	COUNTER=$((COUNTER+1))
 done
 
 COUNTER=0
-while [ $COUNTER -lt 20 ]; do
+while [ $COUNTER -lt 30 ]; do
 	python busters.py -p P3QLearning -l layouts/classic.lay -s
 	COUNTER=$((COUNTER+1))
 done
